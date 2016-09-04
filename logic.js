7e9cfe3e5caef4284cf1f6e2a3c23b234f4dbf90
cubbic <danpopa1998@gmail.com>
2016-09-04 15:10:14 +0300

    test cu ad

---
+++ logic.js
@@ -0,0 +1,29 @@
+function genereazaCuvint(){
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 3; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+
+    return text;
+}
+$( document ).ready(function() {
+    var cuvintGen = genereazaCuvint();
+    var key = 'AIzaSyBy0PhbxUjNGuJ2kabcr9A9eh4QMuwPa2s ';
+    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q='+cuvintGen+'&key=' + key;
+    $.getJSON( url, function( data ) {
+        var json = data.items;
+        json = json.filter(function (obj) {
+          return obj.id.kind == 'youtube#video';
+        });
+
+        var chosen = json[Math.floor(Math.random() * json.length)];
+        var id = chosen.id.videoId;
+        console.log(id);
+        var embedLink = 'https://www.youtube.com/embed/'+ id;
+        $('#videoFrame').attr('src' , embedLink);
+        $('#loading-text').text('');
+    });
+
+});
